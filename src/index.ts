34a6f59d827b4c6055953df3a3e5b0e530eed6d0
Nenad <nenad@better.giving>
2024-04-22 20:50:21 +0200

    Fix logic

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,48 @@
+/** @author misicnenad */
+
 import { extendEnvironment } from "hardhat/config";
 import { createProvider } from "hardhat/internal/core/providers/construction";
-import { EthereumProvider } from "hardhat/types/provider";
+import { lazyFunction } from "hardhat/plugins";
+import type { EthereumProvider } from "hardhat/types/provider";
+import { HardhatEthersProvider } from "@nomicfoundation/hardhat-ethers/internal/hardhat-ethers-provider";
 
 // This import is needed to let the TypeScript compiler know that it should include your type
 // extensions in your npm package's types file.
 import "./type-extensions";
-
+import Web3 from "web3";
 
 extendEnvironment((hre) => {
   // We add a field to the Hardhat Runtime Environment here.
-  // We use lazyObject to avoid initializing things until they are actually
-  // needed.
-  const providers: { [name: string]: EthereumProvider } = {};
+  const providers: { [name: string]: EthereumProvider } = {
+    [hre.network.name]: hre.network.provider,
+  };
 
-  hre.getProvider = function getProvider(name: string): EthereumProvider {
+  async function getProvider(name: string): Promise<EthereumProvider> {
     if (!providers[name]) {
-      providers[name] = createProvider(
-        name,
-        this.config.networks[name],
-        this.config.paths,
-        this.artifacts,
-      );
+      providers[name] = await createProvider(hre.config, name, hre.artifacts);
     }
     return providers[name];
-  };
+  }
 
-  hre.changeNetwork = function changeNetwork(newNetwork: string) {
-    if (!this.config.networks[newNetwork]) {
-      throw new Error(`changeNetwork: Couldn't find network '${newNetwork}'`);
+  hre.changeNetwork = lazyFunction(() => async (networkName: string) => {
+    // check if network config is set
+    if (!hre.config.networks[networkName]) {
+      throw new Error(`changeNetwork: Couldn't find network '${networkName}'`);
     }
 
-    if (!providers[this.network.name]) {
-      providers[this.network.name] = this.network.provider;
-    }
+    const toProvider = await getProvider(networkName);
 
-    this.network.name = newNetwork;
-    this.network.config = this.config.networks[newNetwork];
-    this.network.provider = this.getProvider(newNetwork);
+    // update hardhat's network data
+    hre.network.name = networkName;
+    hre.network.config = hre.config.networks[networkName];
+    hre.network.provider = toProvider;
 
-    if ((this as any).ethers) {
-      const { EthersProviderWrapper } = require("@nomiclabs/hardhat-ethers/internal/ethers-provider-wrapper");
-      (this as any).ethers.provider = new EthersProviderWrapper(this.network.provider);
+    // update underlying library's provider data
+    if (hre.ethers) {
+      hre.ethers.provider = new HardhatEthersProvider(toProvider, networkName);
     }
-  };
+    if (hre.web3) {
+      hre.web3 = new Web3(toProvider);
+    }
+  });
 });
--- src/type-extensions.ts
+++ src/type-extensions.ts
@@ -1,15 +1,9 @@
-// If your plugin extends types from another plugin, you should import the plugin here.
-
-// To extend one of Hardhat's types, you need to import the module where it has been defined, and redeclare it.
 import "hardhat/types/runtime";
-import { EthereumProvider } from "hardhat/types/provider";
 
 declare module "hardhat/types/runtime" {
-
-  // This is an example of an extension to the Hardhat Runtime Environment.
-  // This new field will be available in tasks' actions, scripts, and tests.
+  // This is an extension to the Hardhat Runtime Environment.
+  // These new functions will be available in tasks' actions, scripts, and tests.
   export interface HardhatRuntimeEnvironment {
-    changeNetwork(newNetwork: string): void;
-    getProvider(newNetwork: string): EthereumProvider;
+    changeNetwork(networkName: string): Promise<void>;
   }
 }
