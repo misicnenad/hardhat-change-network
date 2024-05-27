81c673a7e03a8283de0adb66f1523f2e94bb1702
Nenad <nenad@better.giving>
2024-05-27 11:50:04 +0200

    format + remove ./test from rootDirs

--- test/project.test.ts
+++ test/project.test.ts
@@ -3,11 +3,11 @@ import { assert } from "chai";
 import { useEnvironment } from "./helpers";
 
 describe("Integration tests examples", () => {
-	describe("Hardhat Runtime Environment extension", () => {
-		useEnvironment("hardhat-project");
+  describe("Hardhat Runtime Environment extension", () => {
+    useEnvironment("hardhat-project");
 
-		it("Should add the functoin", function () {
-			assert.instanceOf(this.hre.changeNetwork, Function);
-		});
-	});
+    it("Should add the function", function () {
+      assert.instanceOf(this.hre.changeNetwork, Function);
+    });
+  });
 });
